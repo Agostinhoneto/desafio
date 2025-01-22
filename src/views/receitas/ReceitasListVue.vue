7ad2bf71e6a4fe157a95285eb59ffc945f673e19
AgostinhoNeto <agostneto6@gmail.com>
2025-01-22 15:28:30 -0300

    criando lista de receitas

--- src/views/receitas/ReceitasListVue.vue
+++ src/views/receitas/ReceitasListVue.vue
@@ -12,7 +12,7 @@
         <div class="container-fluid">
             <div class="d-flex justify-content-between align-items-center mb-3">
                 <div>
-                    <router-link :to="{ name: 'cad-despesas' }" class="btn btn-success btn-sm">
+                    <router-link :to="{ name: 'cad-receitas' }" class="btn btn-success btn-sm">
                         <i class="fas fa-user-plus"></i> Adicionar Receitas
                     </router-link>
                 </div>
